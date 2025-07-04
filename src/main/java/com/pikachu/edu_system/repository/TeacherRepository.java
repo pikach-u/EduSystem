a725a81fb7dc67e4bd8310415d3eedfa580ffdb8
pikach-u <ziwon05@naver.com>
2025-07-04 13:51:12 +0900

    Edu System TeacherRepository

--- src/main/java/com/pikachu/edu_system/repository/TeacherRepository.java
+++ src/main/java/com/pikachu/edu_system/repository/TeacherRepository.java
@@ -1,10 +1,18 @@
 package com.pikachu.edu_system.repository;
 
+import com.pikachu.edu_system.model.Teacher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class TeacherRepository {
     private final JdbcTemplate jdbcTemplate;
 
+    public int save(Teacher teacher) {
+        return jdbcTemplate.update(
+                "INSERT INTO teacher (name) VALUES (?)", teacher.getName()
+        );
+    }
 }
